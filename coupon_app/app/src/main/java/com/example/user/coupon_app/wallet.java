57f10e9a2496d810395eca58e663ca28e9840cd5
wzmf399444 <e1234573123@gmail.com>
2018-10-22 18:09:40 +0800

    add base navigation class and wallet class

---
+++ coupon_app/app/src/main/java/com/example/user/coupon_app/wallet.java
@@ -0,0 +1,26 @@
+package com.example.user.coupon_app;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by user on 2018/10/22.
+ */
+
+public class wallet extends Navigation_BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wallet);
+//        toolbar.setTitle(R.string.app_name);//設置ToolBar Title
+//        setUpToolBar();//使用父類別的setUpToolBar()，設置ToolBar
+//        CurrentMenuItem = 3;//目前Navigation項目位置
+//        NV.getMenu().getItem(CurrentMenuItem).setChecked(true);//設置Navigation目前項目被選取狀態
+
+    }
+}
+
