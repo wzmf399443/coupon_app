dd3cbbe7d53e6298dcccd5c89411cbce112cec58
splasky <s1101b037@gms.nutc.edu.tw>
2018-11-07 19:00:14 +0800

    Add identity string for login identity

--- coupon_app/app/src/main/java/com/example/user/coupon_app/start_page.java
+++ coupon_app/app/src/main/java/com/example/user/coupon_app/start_page.java
@@ -1,9 +1,11 @@
 package com.example.user.coupon_app;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import com.example.user.coupon_app.Util.Identity;
 
 public class start_page extends AppCompatActivity {
 
@@ -13,11 +15,13 @@ public class start_page extends AppCompatActivity {
         setContentView(R.layout.activity_start_page);
     }
     public void go_cusmer(View v){
+        Identity.setToken(getString(R.string.id_customer));
         Intent cusumer = new Intent();
         cusumer.setClass(start_page.this,login_page.class);
         startActivity(cusumer);
     }
     public void go_store(View v){
+        Identity.setToken(getString(R.string.id_store));
         Intent store = new Intent();
         store.setClass(start_page.this,login_page.class);
         startActivity(store);
